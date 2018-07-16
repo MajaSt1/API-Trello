feb1b84233b55c1a429a80b3157dd34c2cf3c47f
MajaSt1 <ikjam1@gmail.com>
2018-07-16 22:51:49 +0200

    init

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -2,11 +2,14 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class TaskDto {
     private Long id;
     private String title;
     private String content;
+
 }

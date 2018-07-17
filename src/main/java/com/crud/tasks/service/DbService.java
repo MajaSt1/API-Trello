a8b9d2f93488d5185d63ffb8e105537ace17e414
MajaSt1 <ikjam1@gmail.com>
2018-07-17 15:33:31 +0200

    init

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -15,7 +16,10 @@ public class DbService {
     public List<Task> getAllTasks(){
         return taskRepository.findAll();
     }
-    public Task getTaskByID(Long taskId){
-        return taskRepository.findById(taskId);
+    public Optional<Task> getTaskByID(final Long taskId){
+        return taskRepository.findById(taskId); // .orElse(null);
+    }
+    public Task saveTask(final Task task){
+        return taskRepository.save(task);
     }
     }

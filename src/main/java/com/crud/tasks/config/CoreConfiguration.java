ffd62d5bf174895a35e88af3d8aa6c800ff10191
MajaSt1 <ikjam1@gmail.com>
2018-08-08 18:04:33 +0200

    init

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -14,7 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @Configuration
-@ComponentScan
+
 public class CoreConfiguration extends WebMvcConfigurerAdapter {
 
     @Bean

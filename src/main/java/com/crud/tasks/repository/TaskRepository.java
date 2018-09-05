fd4354c7434097f92cc5f1c645c7041f6d0e8806
MajaSt1 <ikjam1@gmail.com>
2018-09-05 18:12:14 +0200

    init

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -19,7 +19,7 @@ public interface TaskRepository extends CrudRepository<Task,Long> {
     Optional<Task> findById(Long id);
 
     @Modifying
-    void deleteById(Long id);
+    void delete(Long id);
 
     @Override
     long count();
